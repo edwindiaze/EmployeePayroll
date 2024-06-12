6ec50bd5ca01730bc203d0990cbdc3cf3d1be57a
edwindiaze <edwin.diaze@perficient.com>
2024-06-12 17:10:19 -0500

    Add Web API layer
    Add Web API layer

---
+++ src/EmployeePayroll.WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using EmployeePayroll.Application.Employees.Commands;
+using EmployeePayroll.Application.Employees.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePayroll.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EmployeesController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(Guid id)
+        {
+            var query = new GetEmployeeQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var query = new GetEmployeesQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetEmployee), new { id }, id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(Guid id, UpdateEmployeeCommand command)
+        {
+            if (id == command.Id)
+            {
+                await _mediator.Send(command);
+                return NoContent();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(Guid id)
+        {
+            var command = new DeleteEmployeeCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
